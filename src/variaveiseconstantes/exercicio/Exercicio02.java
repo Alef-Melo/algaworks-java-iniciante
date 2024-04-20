c03b6f25a3d73d5838604fddbfc389b06fbc111c
Mrblack <alef.andre76@gmail.com>
2024-04-20 18:27:52 -0300

    Exercicio de fixação (tipos de texto e concatenação)

---
+++ src/variaveiseconstantes/exercicio/Exercicio02.java
@@ -0,0 +1,19 @@
+package variaveiseconstantes.exercicio;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.nextLine();
+
+        System.out.println("Olá " + nome + " " + sobrenome + "!");
+
+
+    }
+}
